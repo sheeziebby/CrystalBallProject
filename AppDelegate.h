c04ce805b7c9734299a6c8fc69170dcb19cbafe5
sheeziebby <rh03558@georgiasouthern.edu>
2014-01-17 02:35:08 -0500

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Rashii Henry on 9/28/13.
+//  Copyright (c) 2013 Rashii Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
